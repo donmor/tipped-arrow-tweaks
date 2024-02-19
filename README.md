caf4280b93e0e18178e4edc05f4e064e85b5f4b8
donmor <donmor3000@hotmail.com>
2024-02-19 15:12:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tipped Arrow Tweaks
+A Minecraft mod allowing you to make tipped arrows with potion in brewing stand.
+
+### Summary
+You can now brew any potion into 3 arrows, making them tipped arrows (the glass bottle got consumed as well).
+
+### Copying
+Distributed under Creative Commons Attribution-NonCommercial-NoDerivs 4.0.
---
+++ src/main/java/net/mcreator/tippedarrowtweaks/recipes/brewing/TippedArrowRecipe1BrewingRecipe.java
@@ -0,0 +1,53 @@
+
+package net.mcreator.tippedarrowtweaks.recipes.brewing;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.crafting.Ingredient;
+
+import org.jetbrains.annotations.NotNull;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class TippedArrowRecipe1BrewingRecipe implements IBrewingRecipe {
+	@NotNull private final Ingredient input;
+	@NotNull private final Ingredient ingredient;
+    @NotNull private final ItemStack output;
+    
+	@SubscribeEvent
+	public static void init(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> BrewingRecipeRegistry.addRecipe(new TippedArrowRecipe1BrewingRecipe()));
+	}
+
+	public TippedArrowRecipe1BrewingRecipe() {
+		this.input = Ingredient.of(Items.ARROW);
+		this.ingredient = Ingredient.of(Items.POTION);
+		this.output = new ItemStack(Items.TIPPED_ARROW);
+	}
+
+	@Override
+	public boolean isInput(ItemStack input) {
+		return this.input.test(input);
+	}
+
+	@Override
+	public boolean isIngredient(ItemStack ingredient) {
+		return this.ingredient.test(ingredient);
+	}
+
+	@Override
+	public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+		if (isInput(input) && isIngredient(ingredient)) {
+			ItemStack newArrow =  output.copy();
+			PotionUtils.setPotion(newArrow, PotionUtils.getPotion(ingredient));
+			return newArrow;
+		}
+		return ItemStack.EMPTY;
+	}
+}
